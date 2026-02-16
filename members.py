2cb4f8c0c2b2dde3d841c52346814fbb864b12c4
tinashejambo <jambotinashe@gmail.com>
2026-02-16 12:55:15 +0200

    commit

---
+++ members.py
@@ -0,0 +1,12 @@
+%load_ext sql
+
+
+%%sql member_profiles
+
+%sql SELECT * FROM some_table;
+
+%%sql SELECT * FROM member_profiles
+#CREATE TABLE users (id int, name varchar(20));
+#INSERT INTO users VALUES (1, 'Alice');
+#SELECT * FROM users;
+
